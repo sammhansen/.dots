7866ed1b2c5ed61889404fb5ce85536d0ad07046
Hansen <samhansen.dev@gmail.com>
2025-07-04 13:47:23 +0300

    fix: pipe null stdin to fuzzel

--- fuzzel/scripts/google.sh
+++ fuzzel/scripts/google.sh
@@ -3,10 +3,12 @@
 pkill fuzzel
 
 BROWSER="/usr/bin/chromium"
+PROMPT="󰊭  "
 PLACEHOLDER="Search Google or type URL"
 SEARCHENGINE="https://www.google.com/search?q="
 
-query=$(echo | /usr/bin/fuzzel --dmenu -w 100 -p "> " --lines 0 --placeholder "$PLACEHOLDER")
+query=$(echo | /usr/bin/fuzzel --dmenu -w 100 -p "$PROMPT" --lines 0 --placeholder "$PLACEHOLDER")
+
 
 [ -z "$query" ] && exit 0
 
