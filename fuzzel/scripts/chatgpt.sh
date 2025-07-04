04ca604ddd2287781a655871af776b363fd8539c
Hansen <samhansen.dev@gmail.com>
2025-07-04 13:46:40 +0300

    fix: pipe null stdin to fuzzel

--- fuzzel/scripts/chatgpt.sh
+++ fuzzel/scripts/chatgpt.sh
@@ -4,8 +4,9 @@ pkill fuzzel
 
 BROWSER="/usr/bin/chromium"
 XARGS="/usr/bin/xargs"
+PROMPT="󰻞  "
 PLACEHOLDER="ask chatgpt"
 SEARCHENGINE="https://chat.openai.com/?q={}"
 
-echo | /usr/bin/fuzzel --dmenu -w 100 -p "> " --lines 0 --placeholder "$PLACEHOLDER" | $XARGS -r -I {} $BROWSER --app="$SEARCHENGINE" 
+echo | /usr/bin/fuzzel --dmenu -w 100 -p "$PROMPT" --lines 0 --placeholder "$PLACEHOLDER" | $XARGS -r -I {} $BROWSER --app="$SEARCHENGINE" 
 
