e3d3d5fe1a09b98889a61520e010be7accc08cd4
Hansen <samhansen.dev@gmail.com>
2025-07-08 23:39:47 +0300

    feat(nvim): add flash-nvim

--- nvim/lua/plugs/utils/conform.lua
+++ nvim/lua/plugs/utils/conform.lua
@@ -1,15 +1,25 @@
-require("conform").setup({
-	format_on_save = {
-		timeout_ms = 500,
-		lsp_format = "fallback",
-	},
-	formatters_by_ft = {
-		lua = { "stylua" },
-		python = { "isort", "black" },
-		nix = { "alejandra" },
-		rust = { "rustfmt", lsp_format = "fallback" },
-		javascript = { "prettierd", "prettier", stop_after_first = true },
-		css = { "prettierd", "prettier", stop_after_first = true },
-		scss = { "prettierd", "prettier", stop_after_first = true },
-	},
-})
+return {
+  "stevearc/conform.nvim",
+  opts = {},
+  config = function()
+    require("conform").setup({
+      format_on_save = {
+        timeout_ms = 2000,
+        lsp_format = "fallback",
+      },
+      formatters_by_ft = {
+        lua = { "stylua" },
+        nix = { "alejandra" },
+        cs = { "clang-format" },
+        cpp = { "clang-format" },
+        rust = { "rustfmt", lsp_format = "fallback" },
+        js = { "prettierd" },
+        css = { "prettierd" },
+        scss = { "prettierd" },
+        sh = { "shfmt" },
+        bash = { "shfmt" },
+      },
+    })
+  end,
+
+}
