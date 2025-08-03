c6e67fe80dd1b45d4bd721678edc583e47b000b1
Hansen <samhansen.dev@gmail.com>
2025-08-04 01:13:32 +0300

    refactor: minor changes

---
+++ niri/scripts/clipboard.sh
@@ -0,0 +1,5 @@
+PLACEHOLDER="Search clipboard history"
+PROMPT="󱉨  "
+
+pkill fuzzel 
+cliphist list | fuzzel  --match-mode fzf --dmenu -w 100 -p "$PROMPT" --placeholder="$PLACEHOLDER" | cliphist decode | wl-copy
