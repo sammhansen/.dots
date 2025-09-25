e1163649c8efcf8b087db35c834460c9870eb5ba
Hansen <samhansen.dev@gmail.com>
2025-09-25 19:00:04 +0300

    chore: new nvim config

---
+++ nvim/lua/config/autocmd.lua
@@ -0,0 +1,4 @@
+-- vim.api.nvim_create_autocmd('TextYankPost', {
+--   desc = 'Highlight when yanking text',
+--   group = vim.api.nvim_create_augroup(
+-- })
---
+++ nvim/lua/config/init.lua
@@ -0,0 +1,5 @@
+require("config.lazy")
+require("config.opt")
+require("config.keymap")
+require("config.autocmd")
+require("config.lsp")
